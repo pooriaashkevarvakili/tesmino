9500a8ba7623e16f2c7c3556f59158d2289c8aef
pooria Vakili <pooriavakili09@gmail.com>
2023-12-17 19:09:07 +0330

    update

--- layouts/Footer.vue
+++ layouts/Footer.vue
@@ -1,5 +1,15 @@
 <template>
-  <div class="text-red-400 bg-green-400 text-5xl">pooria</div>
+<footer  style="border-radius: 0 40px 0px 0px" class="footer w-full mt-6 footer-center p-10 bg-primary text-primary-content">
+  <aside>
+     
+    <div class="flex">give me five 
+      <img id="hand" class="w-6" src="../public/hand-0.png"/>
+    </div>
+  </aside> 
+  <nav>
+    
+  </nav>
+</footer>
 </template>
 
 <script>
@@ -9,5 +19,12 @@ export default {
 </script>
 
 <style>
-
+#hand:hover{
+    box-shadow: none;
+    
+    transform: rotate(10deg);
+  
+  /* background-color: red; */
+  
+}
 </style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <Navbar/>
-      <!-- <Footer/> -->
+       <Footer/> 
   </div>
 </template>
 
